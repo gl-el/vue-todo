ea8c2e517ed59b6b21675502a2e6f2afdc777db1
Gleb <g.elenev@gmail.com>
2024-03-15 09:57:14 +0300

    feat: remove unused elements

---
+++ src/components/AppNav.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "AppNav",
+};
+</script>
+
+<template>
+  <header class="header">
+    <nav class="nav">
+      <router-link to="/todo">Todo page</router-link>
+      <router-link to="/form">Form page</router-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  line-height: 1.5;
+  max-height: 100vh;
+  padding: 0;
+}
+
+.nav {
+  width: 100%;
+  padding: 1rem;
+  font-size: 12px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 10px;
+}
+
+.nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+.nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+.nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+.nav a:first-of-type {
+  border: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import TodoPage from "@/pages/TodoPage.vue";
 
 Vue.use(VueRouter);
@@ -11,16 +10,7 @@ const router = new VueRouter({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      redirect: "/todo",
     },
     {
       path: "/todo",
