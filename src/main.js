cbfef3083d9f2fe896a2007d7282c07cb443915c
Gleb Elenev <g.elenev@gmail.com>
2024-03-17 14:54:10 +0300

    chore: vuelidate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
+import Vuelidate from "vuelidate";
 import { store } from "@/store";
 
 import "./assets/main.css";
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
